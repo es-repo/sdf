b9fef61ab1441236cc2b69bdadf96ca8b13a987c
MacBook Pro 14 <macbookpro14@MacBooks-MacBook-Pro.local>
2026-04-10 17:46:39 +0300

    Slow simplex noise animation

--- src/scenes/simplex_noise.rs
+++ src/scenes/simplex_noise.rs
@@ -17,9 +17,10 @@ impl SceneFrame for SimplexNoiseFrame {
     fn get_pixel_color(&self, coord: Vec2<f32>, time: f32) -> Color {
         let mut f;
         let scale = 3.0;
+        let time_scaled = time * 0.2;
 
         if coord.x < 0.0 && coord.y > 0.0 {
-            let coord = coord * scale + time;
+            let coord = coord * scale + time_scaled;
             f = coord.noise_simplex();
         } else {
             let octaves = if coord.x > 0.0 && coord.y > 0.0 {
@@ -30,7 +31,7 @@ impl SceneFrame for SimplexNoiseFrame {
                 4
             };
 
-            let coord = coord * scale + time;
+            let coord = coord * scale + time_scaled;
             f = coord.fbm_rotated(octaves, 0.5, 0.5);
         }
 
