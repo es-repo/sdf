dbb7443b56c01ad29f7bf5de4fce386e422ed765
eugene_s <eugene.github@gmail.com>
2026-04-29 15:14:47 +0300

    Expose Scene3 FBM controls

--- src/scenes/scene_3.rs
+++ src/scenes/scene_3.rs
@@ -5,6 +5,8 @@ use pixels::wgpu::Color;
 #[derive(Clone, Copy)]
 pub struct Scene3Params {
     pub scale: f32,
+    pub amplitude: f32,
+    pub gain: f32,
     pub octaves: u32,
     pub warp_iterations: u32,
 }
@@ -13,6 +15,8 @@ impl Default for Scene3Params {
     fn default() -> Self {
         Self {
             scale: 3.0,
+            amplitude: 0.5,
+            gain: 0.5,
             octaves: 4,
             warp_iterations: 2,
         }
@@ -37,6 +41,8 @@ impl Scene for Scene3 {
 impl ParameterizedScene for Scene3 {
     fn parameters_ui(&mut self, ui: &mut egui::Ui) {
         ui.add(egui::Slider::new(&mut self.params.scale, 0.1..=16.0).text("Scale"));
+        ui.add(egui::Slider::new(&mut self.params.amplitude, 0.0..=2.0).text("Amplitude"));
+        ui.add(egui::Slider::new(&mut self.params.gain, 0.0..=1.0).text("Gain"));
         ui.add(egui::Slider::new(&mut self.params.octaves, 1..=8).text("Octaves"));
         ui.add(egui::Slider::new(&mut self.params.warp_iterations, 1..=8).text("Warp iterations"));
 
@@ -54,7 +60,9 @@ impl SceneFrame for Scene3Frame {
 
         let mut f = 1.0;
         for _i in 0..self.params.warp_iterations {
-            f = coord3d.fbm_rotated(self.params.octaves, 0.5, 0.5, |coord| coord.noise_simplex());
+            f = coord3d.fbm_rotated(self.params.octaves, self.params.amplitude, self.params.gain, |coord| {
+                coord.noise_simplex()
+            });
             coord3d = coord3d + f;
             coord3d = coord3d.sin();
         }
